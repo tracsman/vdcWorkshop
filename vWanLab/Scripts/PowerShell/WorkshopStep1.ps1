aa6a64eba5ebc85806867b736863f876867dd1f9
Jon Ormond <tracsman@hotmail.com>
2019-03-27 19:45:42 -0700

    Add script 1

---
+++ vWanLab/Scripts/PowerShell/WorkshopStep1.ps1
@@ -0,0 +1,95 @@
+﻿#
+# Azure Virtual WAN Workshop
+#
+# Use your corporate credentials to login (your @microsoft.com account) to Azure
+#
+#
+# Step 1 Create a vWAN, a vWAN Hub, and vWAN VPN Gateway
+# Step 2 Create a NetFoundry Virtual Appliance
+# Step 3 Create a Cisco CSR Virtual Appliance
+# Step 4 Connect the two Azure VNets to the vWAN Hub
+# Step 5 Configure and Connect Site 1 (NetFoundry) using the partner experience
+# Step 6 Configure and Connect Site 2 (Cisco) using manual VPN provisioning
+# (Not included) Step 7 Configure and Connect Client01, a Point-to-Site manual VPN connection
+# (Not included) Step 8 Configure and Connect ExpressRoute to vWAN Hub
+# 
+
+# Step 1
+# 1.1 Validate and Initialize
+# 1.2 Create a vWAN
+# 1.3 Create a vWAN Hub
+# 1.4 Create a vWAN VPN Gateway
+#
+
+# 1.1 Validate and Initialize
+# Az Module Test
+$ModCheck = Get-Module Az.Network -ListAvailable
+If ($Null -eq $ModCheck) {
+    Write-Warning "The Az PowerShell module was not found. This script uses the Az modules for PowerShell"
+    Write-Warning "See the blob post for more information at: https://azure.microsoft.com/blog/how-to-migrate-from-azurerm-to-az-in-azure-powershell/"
+    Return
+    }
+
+# Load Initialization Variables
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+If (Test-Path -Path $ScriptDir\init.txt) {
+        Get-Content $ScriptDir\init.txt | Foreach-Object{
+        $var = $_.Split('=')
+        Try {New-Variable -Name $var[0].Trim() -Value $var[1].Trim() -ErrorAction Stop}
+        Catch {Set-Variable -Name $var[0].Trim() -Value $var[1].Trim()}}}
+Else {Write-Warning "init.txt file not found, please change to the directory where these scripts reside ($ScriptDir) and ensure this file is present.";Return}
+
+
+# Non-configurable Variable Initialization (ie don't modify these)
+$ShortRegion = "westus2"
+$RGName = "Company" + $CompanyID + "-Hub01"
+$vWANName = "C" + $CompanyID + "-vWAN01"
+$HubName = "C" + $CompanyID + "-vWAN01-Hub01"
+$HubVPNName = "C" + $CompanyID + "-vWAN01-Hub01-gw-vpn"
+$HubPrefix = "172.16." + $Company10 + ".0/24"
+
+
+# Start nicely
+Write-Host
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Starting step 1, estimated total time 30 minutes" -ForegroundColor Cyan
+
+# Login and permissions check
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Checking login and permissions" -ForegroundColor Cyan
+Try {Get-AzResourceGroup -Name $ResourceGroup -ErrorAction Stop | Out-Null}
+Catch {# Login and set subscription for ARM
+        Write-Host "Logging in to ARM"
+        Try {$Sub = (Set-AzContext -Subscription $subID -ErrorAction Stop).Subscription}
+        Catch {Connect-AzAccount | Out-Null
+                $Sub = (Set-AzContext -Subscription $subID -ErrorAction Stop).Subscription}
+        Write-Host "Current Sub:",$Sub.Name,"(",$Sub.Id,")"
+        Try {Get-AzResourceGroup -Name $ResourceGroup -ErrorAction Stop | Out-Null}
+        Catch {Write-Warning "Permission check failed, ensure company id is set correctly!"
+                Return}
+}
+
+# 1.2 Create a vWAN
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating vWAN" -ForegroundColor Cyan
+Try {$wan = Get-AzVirtualWan -ResourceGroupName $RGName -Name $vWANName -ErrorAction Stop
+     Write-Host "  vWAN exists, skipping"}
+Catch {$wan = New-AzVirtualWan -ResourceGroupName $RGName -Name $vWANName -Location $ShortRegion -AllowBranchToBranchTraffic -AllowVNetToVNetTraffic}
+
+# 1.3 Create a vWAN Hub
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating Hub" -ForegroundColor Cyan
+Try {}
+Catch {}
+
+# 1.4 Create a vWAN VPN Gateway
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Creating VPN Gateway" -ForegroundColor Cyan
+Try {}
+Catch {}
+
+# End nicely
+Write-Host (Get-Date)' - ' -NoNewline
+Write-Host "Step 1 completed successfully" -ForegroundColor Green
+Write-Host "  Wait for the proctor to notify you that your ExpressRoute circuits have been provisioned by the Service Provider before continuing."
+Write-Host
