3d1e876afffb423632cc50f485919d30d433d5a4
Jon Ormond <jonor@microsoft.com>
2019-04-17 22:45:19 -0700

    Update Firewall lab steps 1 and 2

--- Firewall/Scripts/PowerShell/WorkshopStep1.ps1
+++ Firewall/Scripts/PowerShell/WorkshopStep1.ps1
@@ -1,24 +1,21 @@
 ﻿#
-# Azure Virtual WAN Workshop
+# Azure Firewall Workshop
 #
 # Use your corporate credentials to login (your @microsoft.com account) to Azure
 #
 #
-# Step 1 Create a vWAN, a vWAN Hub, and vWAN VPN Gateway
-# Step 2 Create a NetFoundry Virtual Appliance
-# Step 3 Create a Cisco CSR Virtual Appliance
-# Step 4 Connect the two Azure VNets to the vWAN Hub
-# Step 5 Configure and Connect Site 1 (NetFoundry) using the partner experience
-# Step 6 Configure and Connect Site 2 (Cisco) using manual VPN provisioning
-# (Not included) Step 7 Configure and Connect Client01, a Point-to-Site manual VPN connection
-# (Not included) Step 8 Configure and Connect ExpressRoute to vWAN Hub
+# Step 1 Create Virtual Network
+# Step 2 Create an internet facing VM
+# Step 3 Create an ExpressRoute circuit and ER Gateway
+# Step 4 Bring up ExpressRoute Private Peering
+# Step 5 Create the connection between the ER Gateway and the ER Circuit
+# Step 6 Create and configure the Azure Firewall
+# Step 7 Create Spoke VNet with IIS Server and a firewall rule to allow traffic
 # 
 
-# Step 1 - Create a vWAN, a vWAN Hub, and vWAN VPN Gateway
+# Step 1 Create Virtual Network
 # 1.1 Validate and Initialize
-# 1.2 Create a vWAN
-# 1.3 Create a vWAN Hub
-# 1.4 Create a vWAN VPN Gateway
+# 1.2 Create VNet
 #
 
 # 1.1 Validate and Initialize
@@ -26,7 +23,7 @@
 $ModCheck = Get-Module Az.Network -ListAvailable
 If ($Null -eq $ModCheck) {
     Write-Warning "The Az PowerShell module was not found. This script uses the Az modules for PowerShell"
-    Write-Warning "See the blob post for more information at: https://azure.microsoft.com/blog/how-to-migrate-from-azurerm-to-az-in-azure-powershell/"
+    Write-Warning "See the blog post for more information at: https://azure.microsoft.com/blog/how-to-migrate-from-azurerm-to-az-in-azure-powershell/"
     Return
     }
 
@@ -41,14 +38,17 @@ Else {Write-Warning "init.txt file not found, please change to the directory whe
 
 # Non-configurable Variable Initialization (ie don't modify these)
 $ShortRegion = "westus2"
-$RGName = "Company" + $CompanyID + "-Hub01"
-$vWANName = "C" + $CompanyID + "-vWAN01"
-$HubPrefix = "172.16." + $CompanyID + ".0/24"
+$RGName = "Company" + $CompanyID
+$VNetName = "C" + $CompanyID + "-VNet"
+$HubAddress = "10.17." + $CompanyID + ".0/25"
+$snTenant = "10.17." + $CompanyID + ".0/27"
+$snGateway = "10.17." + $CompanyID + ".32/27"
+$snFirewall = "10.17." + $CompanyID + ".64/27"
 
 # Start nicely
 Write-Host
 Write-Host (Get-Date)' - ' -NoNewline
-Write-Host "Starting step 1, estimated total time 30 minutes" -ForegroundColor Cyan
+Write-Host "Starting step 1, estimated total time 5 minutes" -ForegroundColor Cyan
 
 # Login and permissions check
 Write-Host (Get-Date)' - ' -NoNewline
@@ -65,29 +65,22 @@ Catch {# Login and set subscription for ARM
               Return}
 }
 
-# 1.2 Create a vWAN
+# 1.2 Create VNet
 Write-Host (Get-Date)' - ' -NoNewline
-Write-Host "Creating vWAN" -ForegroundColor Cyan
-Try {$wan = Get-AzVirtualWan -ResourceGroupName $RGName -Name $vWANName -ErrorAction Stop
-     Write-Host "  vWAN exists, skipping"}
-Catch {$wan = New-AzVirtualWan -ResourceGroupName $RGName -Name $vWANName -Location $ShortRegion -AllowBranchToBranchTraffic -AllowVNetToVNetTraffic}
-
-# 1.3 Create a vWAN Hub
-Write-Host (Get-Date)' - ' -NoNewline
-Write-Host "Creating vWAN Hub" -ForegroundColor Cyan
-$hub = Get-AzVirtualHub -ResourceGroupName $RGName -Name $vWANName'-Hub01' -ErrorAction Stop
-If ($null -eq $hub) {$hub = New-AzVirtualHub -ResourceGroupName $RGName -Name $vWANName'-Hub01' -Location $ShortRegion -VirtualWan $wan -AddressPrefix $HubPrefix}
-Else {Write-Host "  vWAN Hub exists, skipping"}
-
-# 1.4 Create a vWAN VPN Gateway
-Write-Host (Get-Date)' - ' -NoNewline
-Write-Host "Creating Hub VPN Gateway" -ForegroundColor Cyan
-Try {Get-AzVpnGateway -ResourceGroupName $RGName -Name $vWANName'-Hub01-gw-vpn' -ErrorAction Stop | Out-Null
-     Write-Host "  Hub VPN Gateway exists, skipping"}
-Catch {New-AzVpnGateway -ResourceGroupName $RGName -Name $vWANName'-Hub01-gw-vpn' -VpnGatewayScaleUnit 1 -VirtualHub $hub}
+Write-Host "Creating vnet" -ForegroundColor Cyan
+Try {$vnet = Get-AzVirtualNetwork -ResourceGroupName $RGName -Name $VNetName -ErrorAction Stop
+     Write-Host "  resource exists, skipping"}
+Catch {$vnet = New-AzVirtualNetwork -ResourceGroupName $RGName -Name $VNetName -AddressPrefix $HubAddress -Location $ShortRegion  
+       Write-Host (Get-Date)' - ' -NoNewline
+       Write-Host "Adding subnets" -ForegroundColor Cyan
+       Add-AzVirtualNetworkSubnetConfig -Name "Tenant" -VirtualNetwork $vnet -AddressPrefix $snTenant | Out-Null
+       Add-AzVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $vnet -AddressPrefix $snGateway | Out-Null
+       Add-AzVirtualNetworkSubnetConfig -Name "AzureFirewallSubnet" -VirtualNetwork $vnet -AddressPrefix $snFirewall | Out-Null
+       Set-AzVirtualNetwork -VirtualNetwork $vnet | Out-Null
+       }
 
 # End nicely
 Write-Host (Get-Date)' - ' -NoNewline
 Write-Host "Step 1 completed successfully" -ForegroundColor Green
-Write-Host "  Explore your new vWAN, Hub, and Gateway in the Azure Portal."
+Write-Host "  Explore your new virtual Network in the Azure Portal."
 Write-Host
