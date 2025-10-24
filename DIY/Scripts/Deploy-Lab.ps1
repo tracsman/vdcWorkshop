df6a9d72734261bbcbde435755ad59391b9394a1
Jon Ormond <tracsman@hotmail.com>
2025-10-24 11:18:50 -0500

    Updates Deploy-Lab to force the lab for Tech Summit

--- DIY/Scripts/Deploy-Lab.ps1
+++ DIY/Scripts/Deploy-Lab.ps1
@@ -6,7 +6,7 @@ If (-not ([environment]::GetEnvironmentVariable("AZUREPS_HOST_ENVIRONMENT") -mat
     Write-Host
     Write-Host "FATAL ERROR: Not in an Azure Cloud Shell session" -ForegroundColor Red
     Write-Host
-    Write-Host "This utility and the associcate DIY Lab scripts are designed to be run only from the Azure CloudShell environment."
+    Write-Host "This utility and the associated DIY Lab scripts are designed to be run only from the Azure CloudShell environment."
     Write-Host "Please initiate a CloudShell session from the Azure Portal by doing the following:"
     Write-Host "  Open the Azure Portal at https://portal.azure.com"
     Write-Host "  The icon immediately to the right of the search bar is the Cloud Shell start button"
@@ -17,7 +17,7 @@ If (-not ([environment]::GetEnvironmentVariable("AZUREPS_HOST_ENVIRONMENT") -mat
     Return
 }
 
-Write-Host "Please select the lab you wish to deploy to your cloud shell:"
+<# Write-Host "Please select the lab you wish to deploy to your cloud shell:"
 Write-Host
 Write-Host "   1. Firewall"
 Write-Host "   2. TBD"
@@ -26,10 +26,13 @@ Write-Host "   4. ExpressRoute Resiliency"
 Write-Host "   0. Exit"
 Write-Host
 Write-Host "  Waiting for your selection [0-4]: " -NoNewline
-$MenuItem = $Host.UI.RawUI.ReadKey()
+$UserPrompt = $Host.UI.RawUI.ReadKey()
+$MenuItem = $UserPrompt.Character
 Write-Host
-Write-Host
-switch ($MenuItem.Character) {
+Write-Host #>
+
+$MenuItem = "4" # Force the ER Resiliency lab
+switch ($MenuItem) {
     "1" {Write-Host "Firewall Lab was selected" -ForegroundColor Cyan
          $RGName = "FWLab"
          $Files = @()
@@ -81,15 +84,28 @@ Write-Host (Get-Date)' - ' -NoNewline
 Write-Host "Creating Init File" -ForegroundColor Cyan
 If (-Not (Test-Path $ScriptPath\init.txt)){
      if ($MenuItem.Character -eq "4") {
-          # Prompt user for CompanyID and ensure it's a number between 10 and 99
+          $resourceGroups = Get-AzResourceGroup
           $CompanyID = 0
-          while ($CompanyID -lt 10 -or $CompanyID -gt 99) {
-               Write-Host
-               Write-Host "Please enter a Company ID between 10 and 99: " -NoNewline
-               [int]$CompanyID = Read-Host
+
+          if ($resourceGroups.Count -eq 0) {
+               throw "No resource groups found."
+          }
+          elseif ($resourceGroups.Count -eq 1) {
+               $name = $resourceGroups[0].ResourceGroupName
+               $CompanyID = $name.Substring($name.Length - 2)
+               Write-Output "Your company number is: $CompanyID"
+          }
+          else {
+               Write-Output "Multiple resource groups found. Please specify the two characters you want to use."
+               # Prompt user for CompanyID and ensure it's a number between 10 and 99
+               
+               while ($CompanyID -lt 10 -or $CompanyID -gt 99) {
+                    Write-Host
+                    Write-Host "Please enter a Company ID between 10 and 99: " -NoNewline
+                    [int]$CompanyID = Read-Host
+               }
           }
           $RGName = "Company$CompanyID"
-          
           $FileContent = "SubID=ae914730-743d-4abd-98f8-830d8947e2fb" + "`nRGName=" + $RGName
      } else {
           $FileContent = "SubID=00000000-0000-0000-0000-000000000000" + "`nShortRegion=westus2" + "`nRGName=" + $RGName
